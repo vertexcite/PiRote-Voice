6fb88c140af79c9a220391594ca3b38f56e32487
Randall Britten <3136161+vertexcite@users.noreply.github.com>
2023-03-11 11:36:47 +1300

    Attempted workaround to make Safari/iOS work
    It might be that Safari on iOS behaves the same as
    Chrome on MacOS.
    
    Testing is hard if page not served by HTTPS, hence pushing
    this commit, even though it isn't really well tested.

--- speechRecognition.js
+++ speechRecognition.js
@@ -1,5 +1,7 @@
 const piString = "3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196442881097566593344612847564823378678316527120190914564856692346034861045432664821339360726024914127372458700660631558817488152092096282925409171536436789259036001133053054882046652138414695194151160943305727036575959195309218611738193261179310511854807446237996274956735188575272489122793818301194912983367336244065664308602139494639522473719070217986094370277053921717629317675238467481846766940513200056812714526356082778577134275778960917363717872146844090122495343014654958537105079227968925892354201995611212902196086403441815981362977477130996051870721134999999837297804995105973173281609631859502445945534690830264252230825334468503526193118817101000313783875288658753320838142061717766914730359825349042875546873115956286388235378759375195778185778053217122680661300192787661119590921642019";
-const is_mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+const is_mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+const is_android = /Android/i.test(navigator.userAgent)
+const is_apple = /iPhone|iPad|iPod/i.test(navigator.userAgent)
 const grouping_size = 5;
 const grouping_shift = 2;
 let headstart_count = 0
@@ -21,20 +23,26 @@ if ("webkitSpeechRecognition" in window) {
     console.log("Speech Recognition Error");
   };
   speechRecognition.onend = () => {
-    if(is_mobile) {
+    speech_paused_action(debug="speech recognition ended.")
+    console.log("Speech Recognition Ended")
+  }
+
+  function speech_paused_action(debug="") {
+    // This needs to be idempotent, because it is called from both the stop button (which on some platforms triggers the the onend event)
+    // and it is also called when speech recognition spontaneously stops, or is stopped via the stop button.
+    if(is_android) {
       save_to_results_array(latest_final)
       latest_final = ""
-      render("", results)
     } else {  
       if(results.slice(-interim_transcript.length) != interim_transcript) {
         save_to_results_array(interim_transcript)
-        render("", results);
+        interim_transcript = ""
       }
     }
-
+    const platform_branch = is_android ? "android: " : "not android: "
+    render("", results, debug=platform_branch+debug)
     document.querySelector("#status").style.display = "none";
-    console.log("Speech Recognition Ended");
-  };
+  }
 
   speechRecognition.onresult = (event) => {
     const last_index = event.results.length - 1;
@@ -44,7 +52,7 @@ if ("webkitSpeechRecognition" in window) {
       const chunk = substitutions(latest_transcript);  // remove all whitespace from chunk
       if(chunk.length > 0) {
         latest_final = chunk;
-        render(chunk, results, `latest_transcript: ${latest_transcript}`);
+        render(chunk, results, debug=`latest_transcript: ${latest_transcript}`);
       }
     } else {
 
@@ -56,7 +64,7 @@ if ("webkitSpeechRecognition" in window) {
         }
       }
 
-      render(interim_transcript, results, `raw interim: ${interim_transcript}`);
+      render(interim_transcript, results, debug=`raw interim: ${interim_transcript}`);
     }
   };
 
@@ -66,10 +74,13 @@ if ("webkitSpeechRecognition" in window) {
 
   document.querySelector("#start").onclick = () => {
     speechRecognition.start();
-  };
+  }
+
   document.querySelector("#stop").onclick = () => {
-    speechRecognition.stop();
-  };
+    speech_paused_action(debug="stop button pressed.")
+    speechRecognition.stop()
+  }
+
   document.querySelector("#input_headstart_count").onchange = () => {
     const headstart_count_string = document.querySelector("#input_headstart_count").value
     headstart_count = parseInt(headstart_count_string)
